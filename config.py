607b6994aed62d33e0bd61bd15beae51d50cc688
Bethy Diakabana <bethydiakabana@Bethys-MacBook-Pro.local>
2017-07-12 15:14:57 -0400

    test config based on api urls

---
+++ config.py
@@ -0,0 +1,12 @@
+# List of channels to priorities above all other channels. Lsit them in order
+# ["firstchoice", "secondchoice"] and so on. 
+priority = ["blizzard"]
+
+# The game you would prefer to watch. If none of your priority channels or followed channels are
+# avaialble to watch, the top streamer for this game will be opened
+game = "Overwatch"
+
+# the quality of the stream you'd prefer. I'd suggest picking "best" if you can support high-quality
+# video, and "worst" if you can't. There are other options such as "high", "low", "medium",
+# and "mobile", but they may not always be avaialble depending on the streamer's configuration.
+quality = "best"
